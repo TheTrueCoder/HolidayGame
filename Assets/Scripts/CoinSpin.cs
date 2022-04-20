87bb9cebf570f805536077c7b745340d6894b7f2
TheTrueCoder <thetruecoderman@gmail.com>
2022-04-20 12:14:04 +1200

    Make HUD and Bouncy coin.

--- Assets/Scripts/CoinSpin.cs
+++ Assets/Scripts/CoinSpin.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class CoinSpin : MonoBehaviour
 {
-    public float SpinSpeed = 3;
+    public float SpinSpeed = 12;
+    public float BounceSpeed = 1;
+    public float BounceScale = 1;
+    private Vector3 InitialLocation;
+
+    void Start()
+    {
+        InitialLocation = transform.position;
+    }
+
     // Update is called once per frame
     void Update()
     {
+        // Slowly turn the coin depending on how much time passed.
         transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * SpinSpeed);
-        Mathf.Sin(Time.fixedTime)
+        // Move the coin up and down in a sine wave.
+        float NewPos = Mathf.Sin(Time.fixedTime * BounceSpeed) * BounceScale;
+        transform.position = new Vector3(0, NewPos, 0) + InitialLocation;
     }
 }
--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
-    private int CollectibleCounter = 0;
+    public Text counter;
+    public int CollectibleCounter = 0;
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Collectible")) {
             CollectibleCounter += 1;
+            counter.text = CollectibleCounter.ToString();
             Destroy(other.gameObject);
         }
     }
