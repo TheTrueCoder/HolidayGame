52565f5909b1d029fb3557289313999942d61cc2
TheTrueCoder <thetruecoderman@gmail.com>
2022-04-20 20:25:30 +1200

    Make Gun work.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -8,11 +8,15 @@ public class Projectile : MonoBehaviour
     public float ExplosionStrength = 1;
     public float ExplosionRadius = 100;
 
-    void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Enemy")) {
-            Rigidbody EnemyRB = other.gameObject.GetComponent<Rigidbody>();
+    void OnCollisionEnter(Collision other)
+    {
+        Collider OtherCollider = other.collider;
+        if (OtherCollider.CompareTag("Enemy"))
+        {
+            Rigidbody EnemyRB = OtherCollider.gameObject.GetComponent<Rigidbody>();
             EnemyRB.isKinematic = false;
             EnemyRB.AddExplosionForce(ExplosionStrength, transform.position, ExplosionRadius);
+            Destroy(gameObject);
         }
     }
 }
