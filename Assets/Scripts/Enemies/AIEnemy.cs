39968c0e3a21abf2e1208b5fb49ee3a89b7ce4cb
TheTrueCoder <thetruecoderman@gmail.com>
2022-04-22 09:41:53 +1200

    Implement Health Bar

--- Assets/Scripts/Enemies/AIEnemy.cs
+++ Assets/Scripts/Enemies/AIEnemy.cs
@@ -2,34 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class AIEnemy : Enemy
 {
     public int health = 10;
-    public const float ExplosionStrength = 100;
-    public const float ExplosionRadius = 100;
+    public float explosionStrength = 100;
+    public float explosionRadius = 100;
+    public float deathWaitSeconds = 4;
+    public Slider healthBar;
     private GameObject player;
     private NavMeshAgent agent;
+    private bool alive = true;
 
     void Start()
     {
         // player = FindObjectOfType<PlayerMovementFPS>().gameObject;
         agent = gameObject.GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player");
+        // Set health bar ranges.
+        healthBar.minValue = 0;
+        healthBar.maxValue = health;
+        healthBar.value = health;
     }
     void Update()
     {
-        agent.SetDestination(player.transform.position);
+        if (alive)
+        {
+            agent.SetDestination(player.transform.position);
+        }
     }
 
     public override void TakeDamage(int damage, Transform hitLocation)
     {
         health -= damage;
-        if (health <= 0) {
+        healthBar.value = health;
+        // If the enemy is out of health, kill it.
+        if (health <= 0)
+        {
+            alive = false;
+            agent.enabled = false;
             Rigidbody SelfRB = gameObject.GetComponent<Rigidbody>();
             SelfRB.isKinematic = false;
-            SelfRB.AddExplosionForce(ExplosionStrength, hitLocation.position, ExplosionRadius);
-            Destroy(gameObject);
+            SelfRB.AddExplosionForce(explosionStrength, hitLocation.position, explosionRadius);
+            StartCoroutine(WaitDestroy(deathWaitSeconds, gameObject));
         }
+    }
+
+    IEnumerator WaitDestroy(float seconds, GameObject objectToDestroy)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(objectToDestroy);
     }
 }
