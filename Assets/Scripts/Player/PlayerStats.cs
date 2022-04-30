598c0f93eea2686f119997ba9d235d7ef2fa5bc2
TheTrueCoder <thetruecoderman@gmail.com>
2022-04-30 14:51:13 +1200

    Hide internal public values from editor

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -11,6 +11,7 @@ public class PlayerStats : MonoBehaviour
     public GameObject deathMessage;
     public int health = 1;
     public int requiredCollectables = 1;
+    [HideInInspector]
     public int collectableCounter = 0;
 
     void Start()
